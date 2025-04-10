a6421fc8935b5f3c36c9dafd7f97b797e5da229c
Dominic Holifield <dholifi@purdue.edu>
2025-04-10 13:28:47 -0400

    add localization class - switch chassis to use localization - switch odom to be child of localization

--- include/appa/appa.h
+++ include/appa/appa.h
@@ -9,11 +9,17 @@
 
 namespace appa {
 
+/* Localization */
+class Localization {
+  public:
+    virtual Pose get() const = 0;
+};
+
 /* Odom */
-class Odom {
+class Odom : public Localization {
   private:
     Pose odom_pose = {0.0, 0.0, 0.0};
-    pros::Mutex odom_mutex;
+    mutable pros::Mutex odom_mutex;
     pros::Task* odom_task = nullptr;
 
     Tracker tracker;
@@ -31,8 +37,8 @@ class Odom {
     void task();
     void start();
 
-    Pose get();
-    Pose get_local();
+    Pose get() const override;
+    Pose get_local() const;
     void set(Pose pose);
     void set(Point point, double theta = NAN);
     void set(double x, double y, double theta = NAN);
@@ -48,7 +54,7 @@ class Odom {
 class Chassis {
   private:
     pros::MotorGroup left_motors, right_motors;
-    Odom& odom;
+    Localization& loc;
     Parameters df_params;
     Point prev_speeds = {0.0, 0.0};
     double path_length = 0.0;
@@ -67,7 +73,8 @@ class Chassis {
     std::atomic<bool> debug{false};
 
     Chassis(const std::initializer_list<int8_t>& left_motors,
-            const std::initializer_list<int8_t>& right_motors, Odom& odom, const Config& config);
+            const std::initializer_list<int8_t>& right_motors, Localization& loc,
+            const Config& config);
     ~Chassis();
 
     void task();
